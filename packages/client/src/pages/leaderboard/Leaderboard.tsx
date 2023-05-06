ce80ec41032ba056abfb85f93c7deeb84114d11a
Yulia Gladysheva <yulliagladysheva@gmail.com>
2023-05-06 13:46:31 +0300

    feature/PRA-31: add leaderboard

--- packages/client/src/pages/leaderboard/Leaderboard.tsx
+++ packages/client/src/pages/leaderboard/Leaderboard.tsx
@@ -1,3 +1,39 @@
-export default function Leaderboard() {
-  return <h1>Страница лидерборда</h1>
+import { Avatar, Icon, List, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
+import React, { cloneElement } from "react";
+
+import './leaderboard.less'
+
+function generate(element: React.ReactElement) {
+  return [0, 1, 2, 3, 5].map((value) =>
+    cloneElement(element, {
+      key: value,
+    }),
+  );
 }
+
+export default function Leaderboard() {
+  return (
+    <main className="page leaderboard">
+      <div className="leaderboard__title">Лидерборд</div>
+      <List sx={{ width: '100%' }}>
+        {generate(
+            <ListItem sx={{ display: 'flex', }}>
+              <ListItemAvatar>
+                <Avatar sx={{ width: 50, height: 50, mr: 3 }}>
+                  <Icon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText
+                primary="Имя"
+                secondary='Фамилия'
+              />
+              <ListItemText style={{display:'flex', justifyContent:'flex-end'}}
+                primary="100"
+              />
+            </ListItem>
+        )}
+      </List>
+    </main>
+  )
+}
+
