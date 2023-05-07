b5a5cffa8ccddd250a0d9b8bad1f12bb1f715ecf
alexTaraskina <serzhanina_aleks@mail.ru>
2023-05-07 18:32:54 +0300

    fix registration page

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import { useLocation } from "react-router";
 import MainHeader from './layout/header/MainHeader'
 import Login from './pages/login/Login'
 import Registration from './pages/registration/Registration'
@@ -13,38 +14,26 @@ import { NotFound } from './pages/notFound/NotFound'
 import { GameEnd } from './pages/gameEnd/GameEnd'
 import './App.less'
 
-import { StyledEngineProvider } from '@mui/material/styles'
-import { ThemeProvider } from '@mui/material/styles'
-import { BrowserRouter } from 'react-router-dom'
-import theme from './theme'
-
 function App() {
-  //const { pathname } = useLocation();
+  const { pathname } = useLocation();
 
   return (
-    <StyledEngineProvider injectFirst>
-      <ThemeProvider theme={theme}>
-        <BrowserRouter>
-          <div className="app">
-            {/* {pathname !== '/login' && pathname !== '/registration' && <MainHeader />} */}
-            <MainHeader />
-            <Routes>
-              <Route path="*" element={<NotFound />} />
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/registration" element={<Registration />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/leaderboard" element={<Leaderboard />} />
-              <Route path="/forum" element={<Forum />} />
-              <Route path="/rules" element={<GameRules />} />
-              <Route path="/game/start" element={<GameStart />} />
-              <Route path="/game/end" element={<GameEnd />} />
-              <Route path="/rooms/:roomId" element={<GameRoom />} />
-            </Routes>
-          </div>
-        </BrowserRouter>
-      </ThemeProvider>
-    </StyledEngineProvider>
+    <div className="app">
+      {pathname !== '/login' && pathname !== '/registration' && <MainHeader />}
+      <Routes>
+        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<Home/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/registration" element={<Registration/>} />
+        <Route path="/profile" element={<Profile/>} />
+        <Route path="/leaderboard" element={<Leaderboard/>} />
+        <Route path="/forum" element={<Forum/>} />
+        <Route path="/rules" element={<GameRules/>} />
+        <Route path="/game/start" element={<GameStart />} />
+        <Route path="/game/end" element={<GameEnd/>}/>
+        <Route path="/rooms/:roomId" element={<GameRoom/>} />
+      </Routes>
+    </div>
   );
 }
 
