1c36d64ef3f9058c0274fa2d75420bad47d988da
Шамонова Мария Дмитриевна <shamonova.m@selectel.ru>
2023-05-21 15:14:40 +0300

    fix  tests

--- packages/client/src/pages/game/models/Players.tsx
+++ packages/client/src/pages/game/models/Players.tsx
@@ -2,19 +2,6 @@ import Player from './Player'
 import { IUser } from '../../../api/types'
 
 class Players {
-  init(users: IUser[], callback: (player: Player, index: number) => void) {
-    const players = []
-    for (let index = 0; index < users.length; index++) {
-      players.push(
-        new Player({
-          user: users[index],
-        })
-      )
-      callback(players[index], index)
-    }
-    return players
-  }
-
   getNoActivePlayerIndex(players: Player[], activePlayerId: number): number {
     return players.findIndex(
       (player: Player) => player.user.id !== activePlayerId
