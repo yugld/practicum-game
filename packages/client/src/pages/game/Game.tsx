f0615572d164da6d4b47832bf11b8c2adffb34bd
Шамонова Мария Дмитриевна <shamonova.m@selectel.ru>
2023-05-21 13:06:07 +0300

    fix  websocket message

--- packages/client/src/pages/game/Game.tsx
+++ packages/client/src/pages/game/Game.tsx
@@ -121,11 +121,13 @@ export default function Game({ websocket }: Props) {
 
               return
             }
+            console.log(data)
             if (
               !Array.isArray(data) &&
               data?.content &&
               data?.content?.status === 'confirmStartRound'
             ) {
+              console.log('confirmStartRound')
               console.log(user?.id)
               console.log(data)
               if (user?.id === data?.content?.activePlayer?.user.id) {
