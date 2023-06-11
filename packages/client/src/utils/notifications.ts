368cec1ca513a807a2dfed43ab3543258b0330f8
Шамонова Мария Дмитриевна <shamonova.m@selectel.ru>
2023-06-11 17:19:03 +0300

    fix timeout leaks and fill MEMORYlEAKS.md file

--- packages/client/src/utils/notifications.ts
+++ packages/client/src/utils/notifications.ts
@@ -1,18 +1,22 @@
 export function notifyUser(text: string) {
-    if (!("Notification" in window)) {
-        console.log('Notifications are not supported.');
-    } else if (Notification.permission === 'granted') {
-        showNotification(text);
-    } else if (Notification.permission !== 'denied') {
-        Notification.requestPermission().then((permission) => {
-            if (permission === 'granted') {
-                showNotification(text);
-            }
-        });
-    }
+  if (!('Notification' in window)) {
+    console.log('Notifications are not supported.')
+  } else if (Notification.permission === 'granted') {
+    showNotification(text)
+  } else if (Notification.permission !== 'denied') {
+    Notification.requestPermission().then(permission => {
+      if (permission === 'granted') {
+        showNotification(text)
+      }
+    })
+  }
 }
 
 function showNotification(text: string) {
-    const notification = new Notification(text);
-    setTimeout(notification.close.bind(notification), 3000);
+  const notification = new Notification(text)
+  const timeoutNotification = setTimeout(
+    notification.close.bind(notification),
+    3000
+  )
+  clearTimeout(timeoutNotification)
 }
