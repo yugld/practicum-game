5a3105835a6a036cbaa5f7ef4cffb62543224d39
Yulia Gladysheva <yulliagladysheva@gmail.com>
2023-07-11 19:35:58 +0300

    edit files, add proxyMiddleware

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,10 +1,11 @@
 import dotenv from 'dotenv'
 import cors from 'cors'
-import express, { NextFunction, Request, Response  } from 'express'
+import express from 'express'
 import morgan from 'morgan'
 import { dbConnect } from './db/init'
 import router from './routes/index'
 import cookieParser from 'cookie-parser';
+import { checkAuthMiddleware, proxyMiddleware } from './middleware/checkAuthMiddleware'
 
 if (process.env.NODE_ENV === 'development') {
   dotenv.config({ path: __dirname + '/./../../.env' })
@@ -13,25 +14,18 @@ if (process.env.NODE_ENV === 'development') {
 async function startServer() {
   const app = express()
 
-  app.use(cookieParser())
-
   app.use(cors())
+  app.use(
+    express.urlencoded({
+      extended: true,
+    })
+  )
   app.use(express.json())
-
-  const checkAuthMiddleware = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
-    console.log('checkAuthMiddleware req.cookies.auth: ', req.cookies.auth)
-    const Cookie = await req.cookies.auth
-    if (Cookie) {
-      return next()
-    }
-    res.redirect('/login')
-  }
-
-  app.use('*', checkAuthMiddleware);
+  app.use(cookieParser())
+  
+  app.use('/api/v2', proxyMiddleware())
+  app.use('/api/userRating/top', checkAuthMiddleware)
+  app.use('/api/theme', checkAuthMiddleware)
 
   app.use((req, res, next) => {
     res.setHeader('Content-Type', 'application/json')
